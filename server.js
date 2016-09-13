3ccccfe6a98bd88bb37cfeef881460b81794a0c1
jetilling <jetilling@hotmail.com>
2016-09-13 16:41:44 -0600

    completed everythings except black diamond

--- server.js
+++ server.js
@@ -1,1 +1,52 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 
+
+var app = express();
+
+var messages = ["its working, man",]
+
+app.use(bodyParser.json());
+
+app.options('/', function(req, res, next){
+  res.status(200).set({
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+}).send(JSON.stringify(messages));
+})
+
+app.post('/', function(req, res, next) {
+  console.log(req.body);
+  messages.push({
+    message: req.body.message,
+    time: new Date()
+  });
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+})
+
+app.get('/', function(req, res, next){
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+});
+
+
+
+app.listen(8989, function(){
+  console.log('working')
+})
